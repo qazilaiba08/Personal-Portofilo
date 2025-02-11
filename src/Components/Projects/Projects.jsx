e0bfd4116281008bd0949c07a07c12c6731c47e7
Laiba Qazi <164245574+qazilaiba08@users.noreply.github.com>
2025-02-12 02:32:52 +0500

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,22 +1,4 @@
 
-// import ProjectCards from "./ProjectCards"
-
-
-// const Projects = () => {
-//   return (
-//     <div id="Projects" className="p-10 md:p-24 text-white">
-//         <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
-//         <div className="py-12 px-8 flex flex-wrap gap-5">
-//             <ProjectCards title="Weather Forecast App" main="This is Weather app is created with ReactJS and Bootstrap ✨. " />
-//  <ProjectCards title="Profile Data App" main="This is Profile Data app is created with ReactJS and Bootstrap ✨." />
-//  <ProjectCards title="Todo App" main="This is Todo app is created with ReactJS and Bootstrap ✨ ." />
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Projects  
-
 import ProjectCards from "./ProjectCards"
 
 const Projects = () => {
@@ -24,7 +6,7 @@ const Projects = () => {
     <div id="Projects" className="p-10 md:p-24 text-white">
       <h1 className="text-2xl md:text-4xl font-bold text-center">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap justify-center gap-6">
-        <ProjectCards title="Weather Forecast App" main="A weather app built with ReactJS and Bootstrap. 🌦️" />
+        <ProjectCards title="Weather Forecast App" main="A weather app built with ReactJS and Bootstrap. 🌦️"  />
         <ProjectCards title="Profile Data App" main="An app to manage profile data, created using ReactJS and Bootstrap. 👤" />
         <ProjectCards title="Todo App" main="A simple Todo app made with ReactJS and Bootstrap. ✅" />
       </div>
