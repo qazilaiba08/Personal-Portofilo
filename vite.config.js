d92f52afa3ac6d7d1b76e449acf3b4b10c7b8a6a
qazilaiba08 <laibaqazi721@gmail.com>
2025-07-07 01:53:55 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
-// https://vite.dev/config/
+//  https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
